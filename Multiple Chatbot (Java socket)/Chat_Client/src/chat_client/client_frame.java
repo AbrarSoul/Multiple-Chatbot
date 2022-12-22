edb29f6c595c22bbf1f1d94b687f8e543fc17fa4
AbrarSoul <73701376+AbrarSoul@users.noreply.github.com>
2022-12-23 00:48:51 +0600

    upload

---
+++ Multiple Chatbot (Java socket)/Chat_Client/src/chat_client/client_frame.java
@@ -0,0 +1,331 @@
+package chat_client;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class client_frame extends javax.swing.JFrame 
+{
+    String username, address = "localhost";
+    ArrayList<String> users = new ArrayList();
+    int port = 2222;
+    Boolean isConnected = false;
+    
+    Socket sock;
+    BufferedReader reader;
+    PrintWriter writer;
+    
+    //--------------------------//
+    
+    public void ListenThread() 
+    {
+         Thread IncomingReader = new Thread(new IncomingReader());
+         IncomingReader.start();
+    }
+    
+    //--------------------------//
+    
+    public void userAdd(String data) 
+    {
+         users.add(data);
+    }
+    
+    //--------------------------//
+    
+    public void userRemove(String data) 
+    {
+         ta_chat.append(data + " is now offline.\n");
+    }
+    
+    //--------------------------//
+    
+    public void writeUsers() 
+    {
+         String[] tempList = new String[(users.size())];
+         users.toArray(tempList);
+        /* for (String token:tempList) 
+         {
+             //users.append(token + "\n");
+         }*/
+    }
+    
+    //--------------------------//
+    
+    public void sendDisconnect() 
+    {
+        String bye = (username + ": :Disconnect");
+        try
+        {
+            writer.println(bye); 
+            writer.flush(); 
+        } catch (Exception e) 
+        {
+            ta_chat.append("Could not send Disconnect message.\n");
+        }
+    }
+
+    //--------------------------//
+    
+    public void Disconnect() 
+    {
+        try 
+        {
+            ta_chat.append("Disconnected.\n");
+            sock.close();
+        } catch(Exception ex) {
+            ta_chat.append("Failed to disconnect. \n");
+        }
+        isConnected = false;
+        tf_username.setEditable(true);
+
+    }
+    
+    public client_frame() 
+    {
+        initComponents();
+    }
+    
+    //--------------------------//
+    
+    public class IncomingReader implements Runnable
+    {
+        @Override
+        public void run() 
+        {
+            String[] data;
+            String stream, done = "Done", connect = "Connect", disconnect = "Disconnect", chat = "Chat";
+
+            try 
+            {
+                while ((stream = reader.readLine()) != null) 
+                {
+                     data = stream.split(":");
+
+                     if (data[2].equals(chat)) 
+                     {
+                        ta_chat.append(data[0] + ": " + data[1] + "\n");
+                        ta_chat.setCaretPosition(ta_chat.getDocument().getLength());
+                     } 
+                     else if (data[2].equals(connect))
+                     {
+                        ta_chat.removeAll();
+                        userAdd(data[0]);
+                     } 
+                     else if (data[2].equals(disconnect)) 
+                     {
+                         userRemove(data[0]);
+                     } 
+                     else if (data[2].equals(done)) 
+                     {
+                        //users.setText("");
+                        writeUsers();
+                        users.clear();
+                     }
+                }
+           }catch(Exception ex) { }
+        }
+    }
+
+    //--------------------------//
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lb_username = new javax.swing.JLabel();
+        tf_username = new javax.swing.JTextField();
+        b_connect = new javax.swing.JButton();
+        b_disconnect = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        ta_chat = new javax.swing.JTextArea();
+        tf_chat = new javax.swing.JTextField();
+        b_send = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Chat - Client's frame");
+        setName("client"); // NOI18N
+        setResizable(false);
+
+        lb_username.setText("Username :");
+
+        tf_username.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tf_usernameActionPerformed(evt);
+            }
+        });
+
+        b_connect.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        b_connect.setForeground(new java.awt.Color(0, 255, 0));
+        b_connect.setText("Connect");
+        b_connect.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b_connectActionPerformed(evt);
+            }
+        });
+
+        b_disconnect.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        b_disconnect.setForeground(new java.awt.Color(255, 0, 0));
+        b_disconnect.setText("Disconnect");
+        b_disconnect.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b_disconnectActionPerformed(evt);
+            }
+        });
+
+        ta_chat.setColumns(20);
+        ta_chat.setRows(5);
+        jScrollPane1.setViewportView(ta_chat);
+
+        b_send.setForeground(new java.awt.Color(0, 153, 102));
+        b_send.setText("SEND");
+        b_send.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b_sendActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 20)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 153, 153));
+        jLabel1.setText("Conversations");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 205, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(48, 48, 48)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(tf_username)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(lb_username, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(layout.createSequentialGroup()
+                                            .addComponent(b_connect, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                            .addComponent(b_disconnect)))))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addComponent(tf_chat, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(81, 81, 81)
+                                .addComponent(b_send, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addComponent(jLabel1)))
+                .addContainerGap(19, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 322, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lb_username)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(tf_username, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(b_connect)
+                            .addComponent(b_disconnect))
+                        .addGap(18, 18, 18)
+                        .addComponent(tf_chat, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(b_send)))
+                .addContainerGap(61, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void tf_usernameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tf_usernameActionPerformed
+    
+    }//GEN-LAST:event_tf_usernameActionPerformed
+
+    private void b_connectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_connectActionPerformed
+        if (isConnected == false) 
+        {
+            username = tf_username.getText();
+            tf_username.setEditable(false);
+
+            try 
+            {
+                sock = new Socket(address, port);
+                InputStreamReader streamreader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(streamreader);
+                writer = new PrintWriter(sock.getOutputStream());
+                writer.println(username + ":has connected.:Connect");
+                writer.flush(); // buffer k clear kore dilam
+                isConnected = true; 
+            } 
+            catch (Exception ex) 
+            {
+                ta_chat.append("Cannot Connect! Try Again. \n");
+                tf_username.setEditable(true);
+            }
+            
+            ListenThread();
+            
+        } else if (isConnected == true) 
+        {
+            ta_chat.append("You are already connected. \n");
+        }
+    }//GEN-LAST:event_b_connectActionPerformed
+
+    private void b_disconnectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_disconnectActionPerformed
+        sendDisconnect();
+        Disconnect();
+    }//GEN-LAST:event_b_disconnectActionPerformed
+
+    private void b_sendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_sendActionPerformed
+        String nothing = "";
+        if ((tf_chat.getText()).equals(nothing)) {
+            tf_chat.setText("");
+            tf_chat.requestFocus();
+        } else {
+            try {
+               writer.println(username + ":" + tf_chat.getText() + ":" + "Chat");
+               writer.flush(); // flushes the buffer
+            } catch (Exception ex) {
+                ta_chat.append("Message was not sent. \n");
+            }
+            tf_chat.setText("");
+            tf_chat.requestFocus();
+        }
+
+        tf_chat.setText("");
+        tf_chat.requestFocus();
+    }//GEN-LAST:event_b_sendActionPerformed
+
+    public static void main(String args[]) 
+    {
+        java.awt.EventQueue.invokeLater(new Runnable() 
+        {
+            @Override
+            public void run() 
+            {
+                new client_frame().setVisible(true);
+            }
+        });
+    }
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton b_connect;
+    private javax.swing.JButton b_disconnect;
+    private javax.swing.JButton b_send;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lb_username;
+    private javax.swing.JTextArea ta_chat;
+    private javax.swing.JTextField tf_chat;
+    private javax.swing.JTextField tf_username;
+    // End of variables declaration//GEN-END:variables
+}
